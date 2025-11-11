8cb6e085eb4a5ec0bb8a69e501ba0cbd60626b65
吴俊奇 <wujunqi@wujunqideMacBook-Air.local>
2025-11-11 21:09:41 +0800

    rq CI test

--- experiments/rq1_screen_inconsistency/mutate/insertion.py
+++ experiments/rq1_screen_inconsistency/mutate/insertion.py
@@ -7,7 +7,10 @@ import numpy as np
 from dotenv import load_dotenv
 
 from guipilot.entities import Screen, Widget, Inconsistency, Bbox
-from ..utils import load_screen
+try:
+    from experiments.rq1_screen_inconsistency.utils import load_screen
+except ImportError:  # pragma: no cover
+    from ..utils import load_screen  # type: ignore
 from .utils import sample_p
 
 
